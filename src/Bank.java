5259709a8734cbdcfa940c73ed4bbdda383b84f5
AloYzy <jachym.arkadiusz@gmail.com>
2018-11-01 18:09:34 +0100

    Initial version of bank project

---
+++ src/Bank.java
@@ -0,0 +1,130 @@
+import java.util.Scanner;
+
+public class Bank {
+
+	private Interface ui;
+	private Scanner sc;
+	private double accountBalance;
+	private BankDAO dao;
+
+	public Bank() {
+		ui = new Interface();
+		sc = new Scanner(System.in);
+		accountBalance = 0;
+		dao = new BankDAO();
+	}
+
+	public void show() {
+		ui.showMain();
+
+		boolean flag = true;
+
+		while (flag) {
+			try {
+				int choice = sc.nextInt();
+				sc.nextLine();
+				switch (choice) {
+				case 1:
+					this.login();
+					flag = false;
+					break;
+				case 2:
+					System.out.println("Zapraszamy ponownie.");
+					flag = false;
+					break;
+				default:
+					System.out.println("Nie ma takiej opcji. Spróbuj jeszcze raz.\n");
+					this.show();
+				}
+			} catch (Exception e) {
+				System.out.println("Nie ma takiej opcji. Spróbuj jeszcze raz.\n");
+				sc.next();
+				this.show();
+			}
+		}
+	}
+
+	private void showUi() {
+		ui.show();
+		int choice = sc.nextInt();
+		sc.nextLine();
+
+		switch (choice) {
+		case 1:
+			this.deposit();
+			break;
+		case 2:
+			this.withdraw();
+			break;
+		case 3:
+			this.checkBalance();
+			break;
+		case 4:
+			this.logout();
+			break;
+		default:
+			System.out.println("Nie ma takiej opcji.");
+		}
+	}
+
+	private void login() {
+		System.out.println("Login: ");
+		String name = sc.nextLine();
+		System.out.println("Has³o: ");
+		String password = sc.nextLine();
+
+		if (dao.checkCredentials(name, password)) {
+			this.showUi();
+		} else {
+			System.out.println("Poda³eœ b³êdny login lub has³o.");
+		}
+	}
+
+	private void logout() {
+		System.out.println("Zosta³eœ wylogowany pomyœlnie.");
+	}
+
+	private void deposit() {
+		System.out.println("Jak¹ kwotê chcesz wp³aciæ?");
+
+		try {
+			double sum = sc.nextDouble();
+			if (dao.deposit(sum)) {
+				System.out.print("Transakcja zakoñczona pomyœlnie. ");
+			} else {
+				System.out.println("B³¹d z po³¹czeniem do bazy. Transakcja zakoñczona niepowodzeniem. ");
+			}
+		} catch (Exception e) {
+			System.out.print("Coœ posz³o nie tak. Transakcja zakoñczona niepowodzeniem. ");
+		} finally {
+			sc.nextLine();
+			this.checkBalance();
+		}
+	}
+
+	private void withdraw() {
+		System.out.println("Jak¹ kwotê chcesz wyp³aciæ?");
+
+		try {
+			double sum = sc.nextDouble();
+
+			if (dao.withdraw(sum)) {
+				System.out.print("Transakcja zakoñczona pomyœlnie. ");
+			} else {
+				System.out.print("Nie mo¿na zrealizowaæ transakcji. Brak wystarczaj¹cych œrodków na koncie. ");
+			}
+		} catch (Exception e) {
+			System.out.print("Coœ posz³o nie tak. Transakcja zakoñczona niepowodzeniem. ");
+		} finally {
+			sc.nextLine();
+			this.checkBalance();
+		}
+	}
+
+	private void checkBalance() {
+		accountBalance = dao.checkBalance();
+		System.out.printf("Stan twojego konta wynosi: %.2f \n\n", accountBalance);
+		this.showUi();
+	}
+
+}
